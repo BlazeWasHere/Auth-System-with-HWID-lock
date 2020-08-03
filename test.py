53fdd55e822792863747fdf0651990035c140c70
Blaze <69124640+BlazeWasHere@users.noreply.github.com>
2020-08-03 18:43:30 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-x = '{"response": [{"confirmed": 0, "addr": "14vDeKDPJHdNTFgRDGt3PKcgbu9AmKGhUd", "unconfirmed": 0}]}'
-
-for unconfirmed in json.loads(x)['response']:
-    print(unconfirmed['unconfirmed'])
